01e0f04169b5cd6a39fb739fcabeaaa35f725d0e
steinerAdi <steinerAdi@users.noreply.github.com>
2024-12-13 14:42:40 +0000

    deploy: 684cfed88de4905328e28d8f2b9e3b9dca51f987

---
+++ _static/pdfViewer.js
@@ -0,0 +1,31 @@
+function generateHTML(tag, tagFeatures = "", text = "") {
+  if (tagFeatures) {
+    features = " " + tagFeatures;
+  }
+  else {
+    features = "";
+  }
+
+  return `<${tag}${features}>${text}</${tag}>`;
+}
+
+
+function getPagefromParams() {
+  params = new URLSearchParams(window.location.search);
+  page = params.get("Page");
+  return page;
+}
+
+
+function addPDFTag(id, link, relativePdfjsPath = "", styleSettings = "", additionalClass = "", pageMode = "none", zoom = "auto") {
+  var element = document.getElementById(id);
+  var page = getPagefromParams();
+  pageString = ""
+  if (page) {
+    pageString = `&page=${page}`
+  }
+
+  zoomString = `&zoom=${zoom}`
+
+  element.innerHTML = generateHTML(tag = "iframe", tagFeatures = `class="embedpdf ${additionalClass}" src="${relativePdfjsPath}pdfjs/web/viewer.html?file=${link}#pagemode=${pageMode}${pageString}${zoomString}" allow="fullscreen" style="${styleSettings}"`);
+}
